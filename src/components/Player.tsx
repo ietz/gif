ba8ce2eea0382262780b4662e7a10012ff678140
Tim Pietz <t.pietz@evasys.de>
2022-02-16 23:57:47 +0100

    Add placeholder video

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,35 @@
 import styled from 'styled-components';
 
 const Player = () => {
-  return <Container />
+  return (
+    <Container>
+      <Video muted autoPlay>
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          type="video/mp4"
+        />
+      </Video>
+    </Container>
+  )
 }
 
 const Container = styled.div`
   grid-area: player;
   background-color: #f7f7f8;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  
+  box-sizing: border-box;
+  padding: 2rem;
+  
+  object-fit: scale-down;
 `;
 
 export default Player;
