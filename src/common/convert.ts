a4d6525174b3b0f15d86817cc5b51ea3d0704003
Tim Pietz <t.pietz@evasys.de>
2022-03-20 16:23:39 +0100

    Refactor ffmpeg command builder

--- src/common/convert.ts
+++ src/common/convert.ts
@@ -10,14 +10,8 @@ export class Converter {
 
   load = () => this.ffmpeg.load();
 
-  convert = async ({file, slice, framerate, playbackRate, scaleFactor}: ConvertOptions): Promise<string> => {
+  convert = async ({file, slice, ...imageOptions}: ConvertOptions): Promise<string> => {
     this.ffmpeg.FS('writeFile', file.name, await fetchFile(file));
-    const videoFilter = [
-      `scale=iw*${scaleFactor}:-1`,
-      `setpts=PTS/${playbackRate}`,
-      `fps=${framerate}`,
-      `split[s0][s1];[s0]palettegen=stats_mode=diff[p];[s1][p]paletteuse`,
-    ].join(',')
 
     await this.ffmpeg.run(
       '-ss',
@@ -27,13 +21,22 @@ export class Converter {
       '-i',
       file.name,
       '-vf',
-      videoFilter,
+      this.getVideoImageFilter(imageOptions),
       '-loop',
       '0',
       'output.gif',
     );
     const data = this.ffmpeg.FS('readFile', 'output.gif');
     return URL.createObjectURL(new Blob([data.buffer], {type: 'image/gif'}));
+  }
+
+  private getVideoImageFilter = ({framerate, playbackRate, scaleFactor}: ImageOptions) => {
+    return [
+      `scale=iw*${scaleFactor}:-1`,
+      `setpts=PTS/${playbackRate}`,
+      `fps=${framerate}`,
+      `split[s0][s1];[s0]palettegen=stats_mode=diff[p];[s1][p]paletteuse`,
+    ].join(',')
   }
 }
 
@@ -42,10 +45,13 @@ export const replaceFileExtension = (fileName: string, extension: string): strin
   return `${fileStem}.${extension}`;
 }
 
-export interface ConvertOptions {
-  file: File;
-  slice: VideoSlice;
+interface ImageOptions {
   playbackRate: number;
   scaleFactor: number;
   framerate: number;
+}
+
+export interface ConvertOptions extends ImageOptions {
+  file: File;
+  slice: VideoSlice;
 }
